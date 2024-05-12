9dfd1cc7e10688b53432936eb0b6f2ff5d945719
Pranav <pranavk24h@gmail.com>
2024-05-12 10:17:38 +0530

    changed personal details

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Rohua, Muzaffarpur Bihar, India - 842002 ",
+  phoneNo: "+91 7643 067 263 ",
+  email: "workmpranav@gmail.com",
 };
